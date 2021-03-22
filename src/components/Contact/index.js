aad4021e0f86a6aa540bd2bcf18ff106d4105cc4
Rachel <rachel.thiim@gmail.com>
2021-03-22 18:30:48 -0500

    Finished front-end behavior for form.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,22 +1,38 @@
+import { useState } from 'react';
+
 const ContactForm = () => {
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+  const { name, email, message } = formState;
+
+  const handleChange = ({ target }) => {
+    setFormState({
+      ...formState, 
+      [target.name]: target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formState);
+  }
+
   return (
     <section>
       <h1>Contact me</h1>
-      <form id="contact-form">
+      <form id="contact-form" onSubmit={handleSubmit}>
         {/* name input */}
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" />
+          <input type="text" name="name" defaultValue={name} onChange={handleChange} />
         </div>
         {/* email input */}
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" />
+          <input type="email" name="email" defaultValue={email} onChange={handleChange} />
         </div>
         {/* message text area */}
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" />
+          <textarea name="message" rows="5" defaultValue={message} onChange={handleChange} />
         </div>
         {/* submit button */}
         <button type="submit">Submit</button>
