d104de98e628b86f91b3e3b96b75db03fbbfb331
Rachel <rachel.thiim@gmail.com>
2021-03-22 09:12:16 -0500

    Saving gallery changes before checking out.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import {
   About,
@@ -6,9 +7,23 @@ import {
 } from './components';
 
 const App = () => {
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
-      <Nav />
+      <Nav 
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}/>
       <main>
         <Gallery />
         <About />
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 const styles = {
@@ -6,51 +7,38 @@ const styles = {
   }
 }
 
-const Nav = () => {
-  const categories = [
-    {
-      name: "commercial",
-      description:
-        "Photos of grocery stores, food trucks, and other commercial projects",
-    },
-    { name: "portraits", description: "Portraits of people in my life" },
-    { name: "food", description: "Delicious delicacies" },
-    {
-      name: "landscape",
-      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
-  ];
-
-  function categorySelected(name) {
-    console.log(`${name} clicked`)
-  }
+const Nav = ({ categories, currentCategory, setCurrentCategory }) => {
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
 
   return (
     <header>
       <nav>
         <ul className="flex-row" style={styles.list}>
-        <li>
-        <h2>
-        <a data-testid="link" href="/">
-          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+          <li>
+            <h2>
+              <a data-testid="link" href="/">
+                <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
-      </h2>
-        </li>
+            </h2>
+          </li>
           <li className="mx-2">
             <a data-testid="about" href="#about">
               About Me
         </a>
           </li>
-          <li>
+          <li className="mx-1">
             <span>Contact</span>
           </li>
           {categories.map((category) => (
-            <li
-              onClick={() => categorySelected(category.name)}
-              className="mx-1"
-              key={category.name}
-            >
-              <span>
+            <li 
+              className={`mx-1 ${currentCategory.name === category.name && 'navActive'
+              }`} 
+              key={category.name}>
+              <span onClick={() => {
+                setCurrentCategory(category)
+              }}>
                 {capitalizeFirstLetter(category.name)}
               </span>
             </li>
